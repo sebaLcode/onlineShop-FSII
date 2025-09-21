660e9bde271381537fc2523256be110803d80acb
sebaLcode <seba.lcode@gmail.com>
2025-09-21 04:28:59 -0300

    Se agrega lista de usuarios

---
+++ js/usersData.js
@@ -0,0 +1,63 @@
+// TODO: solo es de prueba, la contraseña debe ser encriptada a futuro.
+let usuarios =[
+    {
+        id: 1,
+        nombre: "Pedrinho Casas",
+        email: "ped@gmail.com",
+        password: "123abcd",
+        telefono: "",
+        region: "Región Metropolitana de Santiago",
+        comuna: "Estación Central",
+        tipoUsuario: "Cliente"
+    },
+    {
+        id: 2,
+        nombre: "Jorge Lopez",
+        email: "jo@duocuc.cl",
+        password: "dsasad",
+        telefono: "",
+        region: "Región Metropolitana de Santiago",
+        comuna: "Ñuñoa",
+        tipoUsuario: "Cliente"
+    },
+    {
+        id: 3,
+        nombre: "Paco Guzman",
+        email: "pac.gu@gmail.com",
+        password: "olaNotoi",
+        telefono: "",
+        region: "Región de la Araucanía",
+        comuna: "Freire",
+        tipoUsuario: "Cliente"
+    },
+    {
+        id: 4,
+        nombre: "Mirta Gutierrez",
+        email: "mir.gu@gmail.com",
+        password: "qtal123",
+        telefono: "",
+        region: "Región de la Araucanía",
+        comuna: "Pucón",
+        tipoUsuario: "Cliente"
+    },
+    {
+        id: 5,
+        nombre: "Roxana Lopez",
+        email: "rox_lopez@gmail.com",
+        password: "vend1234",
+        telefono: "",
+        region: "Región Metropolitana de Santiago",
+        comuna: "San Bernardo",
+        tipoUsuario: "Vendedor"
+    },
+    {
+        id: 6,
+        nombre: "Jorginho Jr",
+        email: "ped@gmail.com",
+        password: "admin123456",
+        telefono: "",
+        region: "Región Metropolitana de Santiago",
+        comuna: "San Bernardo",
+        tipoUsuario: "Administrador"
+    }
+]
