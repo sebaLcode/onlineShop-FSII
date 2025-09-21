f17ffc8e180472d93083944e74661ffeeee36613
sebaLcode <seba.lcode@gmail.com>
2025-09-21 04:29:41 -0300

    Se añaden las validaciones necesarias al register

--- js/register.js
+++ js/register.js
@@ -1,3 +1,74 @@
+const regiones = {
+    "regiones": [
+        {
+            "region": "Arica y Parinacota",
+            "comunas": ["Arica", "Camarones", "Putre", "General Lagos"]
+        },
+        {
+            "region": "Tarapacá",
+            "comunas": ["Iquique", "Alto Hospicio", "Pozo Almonte", "Camiña", "Colchane", "Huara", "Pica"]
+        },
+        {
+            "region": "Antofagasta",
+            "comunas": ["Antofagasta", "Mejillones", "Sierra Gorda", "Taltal", "Calama", "Ollagüe", "San Pedro de Atacama", "Tocopilla", "María Elena"]
+        },
+        {
+            "region": "Atacama",
+            "comunas": ["Copiapó", "Caldera", "Tierra Amarilla", "Chañaral", "Diego de Almagro", "Vallenar", "Alto del Carmen", "Freirina", "Huasco"]
+        },
+        {
+            "region": "Coquimbo",
+            "comunas": ["La Serena", "Coquimbo", "Andacollo", "La Higuera", "Paiguano", "Vicuña", "Illapel", "Canela", "Los Vilos", "Salamanca", "Ovalle", "Combarbalá", "Monte Patria", "Punitaqui", "Río Hurtado"]
+        },
+        {
+            "region": "Valparaíso",
+            "comunas": ["Valparaíso", "Casablanca", "Concón", "Juan Fernández", "Puchuncaví", "Quintero", "Viña del Mar", "Isla de Pascua", "Los Andes", "Calle Larga", "Rinconada", "San Esteban", "La Ligua", "Cabildo", "Papudo", "Petorca", "Zapallar", "Quillota", "Calera", "Hijuelas", "La Cruz", "Nogales", "San Antonio", "Algarrobo", "Cartagena", "El Quisco", "El Tabo", "Santo Domingo", "San Felipe", "Catemu", "Llaillay", "Panquehue", "Putaendo", "Santa María", "Quilpué", "Limache", "Olmué", "Villa Alemana"]
+        },
+        {
+            "region": "Región del Libertador Gral. Bernardo O’Higgins",
+            "comunas": ["Rancagua", "Codegua", "Coinco", "Coltauco", "Doñihue", "Graneros", "Las Cabras", "Machalí", "Malloa", "Mostazal", "Olivar", "Peumo", "Pichidegua", "Quinta de Tilcoco", "Rengo", "Requínoa", "San Vicente", "Pichilemu", "La Estrella", "Litueche", "Marchihue", "Navidad", "Paredones", "San Fernando", "Chépica", "Chimbarongo", "Lolol", "Nancagua", "Palmilla", "Peralillo", "Placilla", "Pumanque", "Santa Cruz"]
+        },
+        {
+            "region": "Región del Maule",
+            "comunas": ["Talca", "Constitución", "Curepto", "Empedrado", "Maule", "Pelarco", "Pencahue", "Río Claro", "San Clemente", "San Rafael", "Cauquenes", "Chanco", "Pelluhue", "Curicó", "Hualañé", "Licantén", "Molina", "Rauco", "Romeral", "Sagrada Familia", "Teno", "Vichuquén", "Linares", "Colbún", "Longaví", "Parral", "Retiro", "San Javier", "Villa Alegre", "Yerbas Buenas"]
+        },
+        {
+            "region": "Región de Ñuble",
+            "comunas": ["Cobquecura", "Coelemu", "Ninhue", "Portezuelo", "Quirihue", "Ránquil", "Treguaco", "Bulnes", "Chillán Viejo", "Chillán", "El Carmen", "Pemuco", "Pinto", "Quillón", "San Ignacio", "Yungay", "Coihueco", "Ñiquén", "San Carlos", "San Fabián", "San Nicolás"]
+        },
+        {
+            "region": "Región del Biobío",
+            "comunas": ["Concepción", "Coronel", "Chiguayante", "Florida", "Hualqui", "Lota", "Penco", "San Pedro de la Paz", "Santa Juana", "Talcahuano", "Tomé", "Hualpén", "Lebu", "Arauco", "Cañete", "Contulmo", "Curanilahue", "Los Álamos", "Tirúa", "Los Ángeles", "Antuco", "Cabrero", "Laja", "Mulchén", "Nacimiento", "Negrete", "Quilaco", "Quilleco", "San Rosendo", "Santa Bárbara", "Tucapel", "Yumbel", "Alto Biobío"]
+        },
+        {
+            "region": "Región de la Araucanía",
+            "comunas": ["Temuco", "Carahue", "Cunco", "Curarrehue", "Freire", "Galvarino", "Gorbea", "Lautaro", "Loncoche", "Melipeuco", "Nueva Imperial", "Padre las Casas", "Perquenco", "Pitrufquén", "Pucón", "Saavedra", "Teodoro Schmidt", "Toltén", "Vilcún", "Villarrica", "Cholchol", "Angol", "Collipulli", "Curacautín", "Ercilla", "Lonquimay", "Los Sauces", "Lumaco", "Purén", "Renaico", "Traiguén", "Victoria"]
+        },
+        {
+            "region": "Región de Los Ríos",
+            "comunas": ["Valdivia", "Corral", "Lanco", "Los Lagos", "Máfil", "Mariquina", "Paillaco", "Panguipulli", "La Unión", "Futrono", "Lago Ranco", "Río Bueno"]
+        },
+        {
+            "region": "Región de Los Lagos",
+            "comunas": ["Puerto Montt", "Calbuco", "Cochamó", "Fresia", "Frutillar", "Los Muermos", "Llanquihue", "Maullín", "Puerto Varas", "Castro", "Ancud", "Chonchi", "Curaco de Vélez", "Dalcahue", "Puqueldón", "Queilén", "Quellón", "Quemchi", "Quinchao", "Osorno", "Puerto Octay", "Purranque", "Puyehue", "Río Negro", "San Juan de la Costa", "San Pablo", "Chaitén", "Futaleufú", "Hualaihué", "Palena"]
+        },
+        {
+            "region": "Región Aisén del Gral. Carlos Ibáñez del Campo",
+            "comunas": ["Coyhaique", "Lago Verde", "Aisén", "Cisnes", "Guaitecas", "Cochrane", "O’Higgins", "Tortel", "Chile Chico", "Río Ibáñez"]
+        },
+        {
+            "region": "Región de Magallanes y de la Antártica Chilena",
+            "comunas": ["Punta Arenas", "Laguna Blanca", "Río Verde", "San Gregorio", "Cabo de Hornos (Ex Navarino)", "Antártica", "Porvenir", "Primavera", "Timaukel", "Natales", "Torres del Paine"]
+        },
+        {
+            "region": "Región Metropolitana de Santiago",
+            "comunas": ["Cerrillos", "Cerro Navia", "Conchalí", "El Bosque", "Estación Central", "Huechuraba", "Independencia", "La Cisterna", "La Florida", "La Granja", "La Pintana", "La Reina", "Las Condes", "Lo Barnechea", "Lo Espejo", "Lo Prado", "Macul", "Maipú", "Ñuñoa", "Pedro Aguirre Cerda", "Peñalolén", "Providencia", "Pudahuel", "Quilicura", "Quinta Normal", "Recoleta", "Renca", "Santiago", "San Joaquín", "San Miguel", "San Ramón", "Vitacura", "Puente Alto", "Pirque", "San José de Maipo", "Colina", "Lampa", "Tiltil", "San Bernardo", "Buin", "Calera de Tango", "Paine", "Melipilla", "Alhué", "Curacaví", "María Pinto", "San Pedro", "Talagante", "El Monte", "Isla de Maipo", "Padre Hurtado", "Peñaflor"]
+        }
+    ]
+}
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("registroForm");
 
@@ -20,24 +91,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Validaciones en tiempo real
     nombre.addEventListener("input", () => {
-        esValido = nombre.value.trim().length > 0 && nombre.value.length<=150;
+        esValido = nombre.value.trim().length > 0 && nombre.value.length <= 150;
         validarCampo(nombre, esValido);
     });
 
     // Validaciones para el Email
     email.addEventListener("input", () => {
-        
+
         const regex = /^[a-zA-Z0-9._%+-]+@(gmail\.com|duoc\.cl|profesor\.duoc\.cl)$/;
         console.log(email.value.lenght)
         const esValido = regex.test(email.value) && email.value.length <= 100
 
         validarCampo(email, esValido);
     });
-    
+
 
     // Validaciones para la contraseña y confirmar contraseña
     password.addEventListener("input", () => {
-        const esValida = password.value.length >= 4 && password.value.length <=10
+        const esValida = password.value.length >= 4 && password.value.length <= 10
         validarCampo(password, esValida);
         validarCampo(passwordConfirm, password.value === passwordConfirm.value && passwordConfirm.value.length > 0);
     });
@@ -47,4 +118,56 @@ document.addEventListener("DOMContentLoaded", () => {
         validarCampo(passwordConfirm, password.value === passwordConfirm.value && passwordConfirm.value.length > 0);
     });
 
+    const regionSelect = document.getElementById("region");
+    const comunaSelect = document.getElementById("comuna");
+    // Cargando regiones
+    regiones.regiones.forEach(r => {
+        const option = document.createElement("option");
+        option.value = r.region;
+        option.textContent = r.region;
+        regionSelect.appendChild(option);
+    });
+
+    // Al seleccionar región, cargar comunas
+    regionSelect.addEventListener("change", () => {
+        comunaSelect.innerHTML = "<option value=''>Seleccionar comuna</option>";
+        const regionSeleccionada = regiones.regiones.find(r => r.region === regionSelect.value);
+
+        if (regionSeleccionada) {
+            regionSeleccionada.comunas.forEach(comuna => {
+                const option = document.createElement("option");
+                option.value = comuna;
+                option.textContent = comuna;
+                comunaSelect.appendChild(option);
+            });
+        }
+    });
+
+
+    // const btnRegister = document.getElementById("btnRegister");
+
+    // Validación al enviar
+    form.addEventListener("submit", (e) => {
+        e.preventDefault(); // evita enviar si hay errores
+
+        const nombreValido = nombre.value.trim().length > 0 && nombre.value.length <= 150;
+        const regex = /^[a-zA-Z0-9._%+-]+@(gmail\.com|duoc\.cl|profesor\.duoc\.cl)$/;
+        const emailValido = regex.test(email.value) && email.value.length <= 100;
+        const passwordValida = password.value.length >= 4 && password.value.length <= 10;
+        const samePassword = password.value === passwordConfirm.value;
+        const regionValida = regionSelect.value !== "";
+        const comunaValida = comunaSelect.value !== "";
+
+        // TODO: Falta validar que el email no exista
+        if (nombreValido && emailValido && passwordValida && samePassword && regionValida && comunaValida) {
+            alert("Usuario registrado correctamente");
+
+            // TODO: acá falta agregar el usuario a lista, localStorage o BD
+
+            form.reset();
+            document.querySelectorAll(".form-control").forEach(i => i.classList.remove("is-valid", "is-invalid"));
+        } else {
+            alert("Errores en los campos");
+        }
+    });
 });
